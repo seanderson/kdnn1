8b848016a6c678d6346d6086d83210f02a3c0040
Sven Anderson <sanderso@bard.edu>
2024-11-26 07:31:53 -0500

    Create Kdnn.java
    initial version

---
+++ Kdnn.java
@@ -0,0 +1,17 @@
+public class Kdnn {
+ public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("USAGE: Kdnn fileOfDoubles");
+            System.exit(-1);
+        }
+        ArrayList<double[]> dat = null;
+        try {
+            dat = load(args[0]);
+            System.out.println(dat.get(0)[0]);
+        } catch (Exception ex) {
+            System.err.println(ex);
+            System.exit(-1);
+        }
+}
+
+}
